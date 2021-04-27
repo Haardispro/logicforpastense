53c1cc1091dd338da9a1334728392f97e26962ff
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-04-27 10:59:39 +0530

    See this

---
+++ exp.py
@@ -0,0 +1,14 @@
+
+import pastense
+x = str(input("Enter a word:"))
+w = pastense.Past(x)
+
+#print(w.past)
+
+"""
+def f(v):
+    T,x,m='aeiou',"ed",v[-1];return[[[v+x,v+m+x][v[-2]in T and m and v[-3]not in T],[v+x,v[:-1]+"ied"][v[-2]not in T]][m=='y'],v+"d"][m=='e']
+    
+print(f("go"))
+
+"""
